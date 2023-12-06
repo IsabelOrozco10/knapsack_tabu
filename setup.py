4bb6d7aa3d163600e6cbef71a294bd23961a1583
IsabelOrozco10 <141699947+IsabelOrozco10@users.noreply.github.com>
2023-12-06 14:33:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='knapsack_tabu',
       version='0.1',
-      description='A Python package for something which implements the taboo search algorithm to solve the Knapsack Problem',
+      description='A Python package for something which implements the tabu search algorithm to solve the Knapsack Problem',
       url='http://github.com/rvf0068/minimal',
       author=' Alejandro Bigvai,Daira Yalín Hernández Cortés, Isabel Orozco Chavez, Marcos Jesús Nicolás Romero, Yadhira Villeda Trejo',
       author_email='ve403484@uaeh.edu.mx, he354078@uaeh.edu.mx, or421454@uaeh.edu.mx, ni333297@uaeh.edu.mx, vi407515@uaeh.edu.mx',
