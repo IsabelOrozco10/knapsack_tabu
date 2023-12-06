50734bd43982d8b1c76975c264ad0a288dc1cf22
IsabelOrozco10 <141699947+IsabelOrozco10@users.noreply.github.com>
2023-12-06 14:38:58 -0600

    Update __init__.py

--- knapsack_tabu/__init__.py
+++ knapsack_tabu/__init__.py
@@ -1,13 +1,2 @@
-def my_sum(x, y):
-    """A function that sums. """
-    return x+y
 
-
-def my_mul(x, y):
-    """A function that multiply. """
-    return x*y
-
-def my_res(x, y):
-    """A function that multiply. """
-    return x-y
     
