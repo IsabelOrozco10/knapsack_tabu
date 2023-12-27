777a78193dfa8e84240333620d79ac2fa2fa1746
MarcosJesusNicolasR <141697477+MarcosJesusNicolasR@users.noreply.github.com>
2023-12-27 16:29:20 -0600

    Correcion del codigo
    Codigo totalmete corregido hasta el metodo de la busqueda tabu

--- knapsack_tabu/__init__.py
+++ knapsack_tabu/__init__.py
@@ -11,20 +11,14 @@ class ProblemaMochila:
     def vector_ítems(self, vector):
         return [self.ítems[i] for i in range(len(vector)) if vector[i] == 1]
 
-    def vector_utilidades(self):
-        return np.array([list(self.utilidades.values())[i] for i in range(len(self.utilidades))])
-
-    def vector_pesos(self):
-        return np.array([list(self.pesos.values())[i] for i in range(len(self.pesos))])
-
-    def generar_solucion_aleatoria(num_items):
+    def generar_solucion_aleatoria(self):
         return np.array([random.randint(0, 1) for _ in range(self.num_items)])
 
     def mejor_cambio(self, indices, v_actual):
         i_best = indices[0]
-        u_best = (self.utilidades[0]/self.pesos[0])**(v_actual[0])
+        u_best = (self.utilidades[0]/self.pesos[0])**(v_actual[i_best])
         for i in indices:
-            u_i = (self.utilidades[i]/self.pesos)**(v_actual[i])
+            u_i = (self.utilidades[i]/self.pesos[i])**(v_actual[i])
             if u_i > u_best:
                 i_best = i
         return i_best
@@ -32,33 +26,29 @@ class ProblemaMochila:
     def utilidad(self, vector):
         return self.utilidades.dot(vector)
     
-    def tabu_search(self, capacidad_mochila, max_iter=20, L=8):
+    def tabu_search(self, max_iter=20, L=8):
         tabu_list = []
         c = 1
-        X = generar_solucion_aleatoria(self.num_items)
-        Curl_W = X_best.dot(self.pesos)
+        X = self.generar_solucion_aleatoria()
         X_best = X
+        Curl_W = X_best.dot(self.pesos)
         while c<=max_iter:
-            N = [i for i in range(num_items)]
+            N = [i for i in range(self.num_items)]
             start = max([0, c-L])
-            for i in range(start, c)
+            for i in range(start, c):
                 N = N.remove(tabu_list[i])
             for i in N:
-                if (X[i] == 0) and (Cur_W + self.pesos[i] > capacidad_mochila):
+                if (X[i] == 0) and (Curl_W + self.pesos[i] > self.límite):
                     N = N.remove(i)
             if len(N) != 0:
-                i_posible = mejor_cambio(N, X)
+                i_posible = self.mejor_cambio(N, X)
                 tabu_list.append(i_posible)
                 X[i_posible] = 1-X[i_posible]
                 if X[i_posible] == 1:
                     Curl_W = Curl_W + self.pesos[i_posible]
                 else:
                     Curl_W = Curl_W - self.pesos[i_posible]
-            if utilidad(X) > utilidad(X_best):
+            if self.utilidad(X) > self.utilidad(X_best):
                 X_best = X
             c += 1
         return X_best
-                
-                
-
-    
