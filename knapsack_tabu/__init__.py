d1bc7339febf8e59ccbc772f16260e91a9130182
MarcosJesusNicolasR <141697477+MarcosJesusNicolasR@users.noreply.github.com>
2023-12-27 15:43:02 -0600

    Actualización del codigo de la funcion tabu_list
    Se completo el algoritmo de acuerdo al libro.

--- knapsack_tabu/__init__.py
+++ knapsack_tabu/__init__.py
@@ -9,7 +9,7 @@ class ProblemaMochila:
         self.num_items = len(items)
 
     def vector_ítems(self, vector):
-        return {self.ítems[i] for i in range(len(vector)) if vector[i] == 1}
+        return [self.ítems[i] for i in range(len(vector)) if vector[i] == 1]
 
     def vector_utilidades(self):
         return np.array([list(self.utilidades.values())[i] for i in range(len(self.utilidades))])
@@ -20,26 +20,45 @@ class ProblemaMochila:
     def generar_solucion_aleatoria(num_items):
         return np.array([random.randint(0, 1) for _ in range(num_items)])
 
-    def encuentra_mejor_cambio(self, vector_actual, indices):
-        i_best = indice[0]
+    def mejor_cambio(self, indices, v_actual):
+        i_best = indices[0]
+        u_best = (self.utilidades[0]/self.pesos[0])**(v_atual[0])
         for i in indices:
-            
-
-    def tabu_search(self.pesos, self.utilidades, capacidad_mochila, max_iter=200, Life=8):
+            u_i = (self.utilidades[i]/self.pesos)**(v_actual[i])
+            if u_i > u_best:
+                i_best = i
+        return i_best
+    
+    def utilidad(self, vector):
+        return self.utilidades.dot(vector)
+    
+    def tabu_search(self, self.pesos, self.utilidades, capacidad_mochila, max_iter=20, L=8):
         tabu_list = []
         c = 1
-        X_best = generar_solucion_aleatoria(self.num_items)
-        Curl_W = X_best.dot(pesos)
-        X = X_best
+        X = generar_solucion_aleatoria(self.num_items)
+        Curl_W = X_best.dot(self.pesos)
+        X_best = X
         while c<=max_iter:
             N = [i for i in range(num_items)]
-            start = max([0, c-Life])
+            start = max([0, c-L])
             for i in range(start, c)
-                N = N.remove(tabu_list[j])
+                N = N.remove(tabu_list[i])
             for i in N:
-                if (X[i] == 0) and (Cur_W + pesos[i] > capacidad_mochila):
+                if (X[i] == 0) and (Cur_W + self.pesos[i] > capacidad_mochila):
                     N = N.remove(i)
-            if N != null:
+            if len(N) != 0:
+                i_posible = mejor_cambio(N, X)
+                tabu_list.append(i_posible)
+                X[i_posible] = 1-X[i_posible]
+                if X[i_posible] = 1:
+                    Curl_W = Curl_W + self.pesos[i_posible]
+                else:
+                    Curl_W = Curl_W - self.pesos[i_posible]
+            if utilidad(X) > utilidad(X_best):
+                X_best = X
+            c += 1
+        return X_best
+                
                 
 
     
